423ab244f123c8fecc8b5119511322ed8078a5c9
TheDeafOne <woodburnkb20@gcc.edu>
2022-06-06 17:31:44 -0400

    restarting cause I hate myself

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/HomePage/Home";
-import Skills from "./pages/SkillsPage/Skills";
-import Projects from "./pages/ProjectsPage/Projects";
-import About from "./pages/AboutPage/About";
-import NoPage from "./pages/ErrorPage/NoPage";
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App';
 
-export default function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="home" element={<About /> } />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
