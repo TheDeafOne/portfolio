7523569a1418fbf1cc32df45cc26b0b6f8a9280d
TheDeafOne <woodburnkb20@gcc.edu>
2022-06-06 23:08:33 -0400

    Side bar components

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { 
+    SidebarContainer,
+    Icon,
+    CloseIcon
+} from './SidebarElements';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+        <Icon>
+            <CloseIcon />
+        </Icon>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
