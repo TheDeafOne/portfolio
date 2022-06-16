6fb440b0c2023ab0b9fda853316205308bbf4639
TheDeafOne <woodburnkb20@gcc.edu>
2022-06-15 21:16:09 -0400

    Data input

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Fuck',
+    headLine: 'Fartsin',
+    description: 'shitting',
+    buttonLabel: 'begin pissing',
+    imgStart: false,
+    img: require('../../images/dellingussy.jpg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
