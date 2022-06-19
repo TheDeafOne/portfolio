49bf7900ffec97903c97aa9c53b180d8df7a17b8
TheDeafOne <woodburnkb20@gcc.edu>
2022-06-18 22:55:42 -0400

    project section skeleton

---
+++ src/components/ProjectsSection/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Icon1 from '../../images/dellingussy.jpg';
+import Icon2 from '../../images/dellingussy.jpg';
+import Icon3 from '../../images/dellingussy.jpg';
+import {
+    ProjectsContainer,
+    ProjectsWrapper,
+    ProjectsH1,
+    ProjectsH2,
+    ProjectsP,
+    ProjectCard,
+    ProjectsIcon
+} from './ProjectElements';
+const Projects = () => {
+  return (
+    <ProjectsContainer id="projects">
+        <ProjectsH1>Projects</ProjectsH1>
+        <ProjectsWrapper>
+            <ProjectCard>
+                <ProjectsIcon src={Icon1}/>
+                <ProjectsH2>piss</ProjectsH2>
+                <ProjectsP>we be pissing</ProjectsP>
+            </ProjectCard>
+            <ProjectCard>
+                <ProjectsIcon src={Icon2}/>
+                <ProjectsH2>shit</ProjectsH2>
+                <ProjectsP>we be shitting</ProjectsP>
+            </ProjectCard>
+            <ProjectCard>
+                <ProjectsIcon src={Icon3}/>
+                <ProjectsH2>fuck</ProjectsH2>
+                <ProjectsP>we be fucking</ProjectsP>
+            </ProjectCard>
+        </ProjectsWrapper>
+    </ProjectsContainer>
+  )
+}
+
+export default Projects
