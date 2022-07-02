b1566eaf10ea3b88f8570094f3e4b2c45fec6f11
TheDeafOne <woodburnkb20@gcc.edu>
2022-07-01 20:29:32 -0400

    skills svgs and import logic

---
+++ src/images/index.js
@@ -0,0 +1,45 @@
+import BigqueryLogo from './Logos/bigquery-icon.svg';
+import BigtableLogo from './Logos/bigtable-icon.svg';
+import CLogo from './Logos/c-icon.svg';
+import CppLogo from './Logos/cpp-icon.svg';
+import CssLogo from './Logos/css-icon.svg';
+import GitLogo from './Logos/git-icon.svg';
+import HadoopLogo from './Logos/hadoop-icon.svg';
+import HtmlLogo from './Logos/html-icon.svg';
+import JavaLogo from './Logos/java-icon.svg';
+import JavascriptLogo from './Logos/javascript-icon.svg';
+import MatlabLogo from './Logos/matlab-icon.svg';
+import MongodbLogo1 from './Logos/mongodb1-icon.svg';
+import MongodbLogo2 from './Logos/mongodb2-icon.svg';
+import MysqlLogo1 from './Logos/mysql1-icon.svg';
+import MysqlLogo2 from './Logos/mysql-icon.svg';
+import NodejsLogo from './Logos/nodejs-icon.svg';
+import PostgresqlLogo from './Logos/postgresql-icon.svg';
+import PythonLogo from './Logos/python-icon.svg';
+import ReactLogo from './Logos/react-icon.svg';
+import SparkLogo from './Logos/spark-icon.svg';
+import TypescriptLogo from './Logos/typescript-icon.svg';
+
+export {
+    BigqueryLogo,
+    BigtableLogo,
+    CLogo,
+    CppLogo,
+    CssLogo,
+    GitLogo,
+    HadoopLogo,
+    HtmlLogo,
+    JavaLogo,
+    JavascriptLogo,
+    MatlabLogo,
+    MongodbLogo1,
+    MongodbLogo2,
+    MysqlLogo1,
+    MysqlLogo2,
+    NodejsLogo,
+    PostgresqlLogo,
+    PythonLogo,
+    ReactLogo,
+    SparkLogo,
+    TypescriptLogo
+}
