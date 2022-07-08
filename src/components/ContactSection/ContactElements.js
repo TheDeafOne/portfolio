3a29c2ab339c7adfee92e598320253a76cf43c88
TheDeafOne <woodburnkb20@gcc.edu>
2022-07-08 19:28:58 -0400

    styling and start of regex handling

---
+++ src/components/ContactSection/ContactElements.js
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+
+export const ContactContainer = styled.div`
+    background-color: black;
+    padding-top: 40px;
+    padding-bottom: 40px;
+`
+
+export const InfoSection = styled.div`
+    height: 30%;
+    width: 100%;
+`
+
+export const InfoLink = styled.a`
+    margin-left: 6px;
+    color: lightblue;
+`
+
+export const InfoH1 = styled.h1`
+    text-align: center;
+    margin-bottom: 10px;
+    color: #fff;
+`
+
+export const InfoH2 = styled.h2`
+    text-align: center;
+    margin-bottom: 5px;
+    color: #fff;
+`
+
+export const InfoH3 = styled.h3`
+    margin-top: 20px;
+    text-align: center;
+    color: #fff;
+`
+
+export const FormSection = styled.div`
+    width: 100%;
+    padding-top: 50px;
+    display: flex;
+    justify-content: center;
+`
+
+export const FormWrapper = styled.div`
+    display: flex;
+    justify-content: center;    
+`
+
+export const FormColumn1 = styled.div`
+    margin-right: 20px;
+`
+
+export const FormColumn2 = styled.div`
+    height: 100%;
+    width: 35%;
+`
+
+
+export const ContactForm = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
+
+export const ContactInput = styled.input`
+    padding: 10px 20px 10px 15px;
+    margin-top: 0px;
+    margin-bottom: 30px;
+    font-size: 20px;
+    line-height: 20px;
+    width: 500px;
+    border-radius: 5px;
+    border: 1px solid gray;
+    outline: none;
+`
+
+export const ContactFormSubmit = styled.input`
+    width: 120px;
+    height: 50px;
+    margin: 10px auto 0 auto;
+`
+
+export const ContactMessage = styled.textarea`
+    min-height: 194px;
+    width: 100%;
+    font-size: 20px;
+    padding-left: 10px;
+    padding-top: 5px;
+    margin-bottom: 30px;
+    border-radius: 5px;
+    outline: none;
+    resize: vertical;
+`
+
+export const ContactFormError = styled.li`
+    color: #ffffff;
+    margin-top: -26px;
+    margin-bottom: 16px;
+    margin-left: 5px;
+    color: red;
+`
+
+export const ContactFormTip = styled.p`
+    color: white;
+`
+
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { init, sendForm } from 'emailjs-com';
-init('ID')
+import {
+  ContactContainer,
+  InfoSection,
+  InfoH1,
+  InfoH2,
+  InfoH3,
+  InfoLink,
+  FormSection,
+  FormWrapper,
+  FormColumn1,
+  FormColumn2,
+  ContactForm,
+  ContactInput,
+  ContactMessage,
+  ContactFormError,
+  ContactFormTip,
+  ContactFormSubmit
+} from './ContactElements';
+init('pbMBicGKWnIczBhTP')
 
 const ContactSection = () => {
   const [contactNumber, setContactNumber] = useState('000000');
+  const [phoneNumber, setPhoneNumber] = useState();
 
+  
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = d => {
     generateContactNumber();
@@ -16,65 +36,99 @@ const ContactSection = () => {
       console.log('FAILED...', error);
     });
   };
-
+  
   const generateContactNumber = () => {
     const numStr = '000000' + (Math.random() * 1000000 | 0);
     setContactNumber(numStr.substring(numStr.length-6));
   }
-
+  
+  const handlePhoneChange = (value) => {
+    if (value === 'a') {
+      setPhoneNumber(value);
+    }
+  }
+  
   const message = watch('message') || "";
-  const messageCharsLeft = 1500 - message.length;
-
-
+  const messageCharsLeft = message.length;
 
   return (
-    <div id='contact'>
-      <h1>
-        <span>Contact Me</span>
-      </h1>
-      <form 
-        id='contact-form'
-        onSubmit={handleSubmit(onSubmit)}>
-        <input 
-          type='text' 
-          name='user_name' 
-          placeholder='Your Name' 
-          maxLength='30'
-          aria-invalid={errors.user_name ? 'true' : 'false'}
-          {...register('user_name', {required: 'required'})}/><br />
-        {errors.user_name && errors.user_name.type === 'required' && errors.user_name !== undefined && (
-          <div>Name is required<br/></div>
-        )}
-        <input 
-          type='text' 
-          name='user_email' 
-          placeholder='Your Email' 
-          aria-invalid={errors.user_email ? 'true' : 'false'}
-          {...register('user_email', {required: 'required'})}/><br />
-        {errors.user_email && errors.user_email.type === 'required' && errors.user_email !== undefined && (
-          <div>email is required<br/></div>
-        )}
-        <input 
-          type='text' 
-          name='user_phone' 
-          placeholder='Your Phone Number' 
-          aria-invalid={errors.user_phone ? 'true' : 'false'}
-          {...register}/><br />
-        <input 
-          type='text' 
-          name='message' 
-          placeholder='message' 
-          maxLength='1500'
-          aria-invalid={errors.message ? 'true' : 'false'}
-          {...register('message', {required: 'required'})}/><br />
-        {errors.message && errors.message.type === 'required' && errors.message !== undefined && (
-          <div>message is required<br/></div>
-        )}
-        <p>{messageCharsLeft}</p>
-        <input type='submit' value='Send' />
-        <input type='hidden' name='contact_number' value={contactNumber} />
-      </form>
-    </div>
+    <ContactContainer id='contact'>
+      <InfoSection>
+        <InfoH1>
+          Contact Me <br/>
+        </InfoH1>
+        <InfoH2>
+          Email: 
+          <InfoLink href="mailto:woodburnkb20@gcc.edu">
+            woodburnkb20@gcc.edu
+          </InfoLink>  or 
+          <InfoLink href="mailto:keegan.woodburn@gmail.com">
+            keegan.woodburn@gmail.com
+          </InfoLink>
+        </InfoH2>
+        <InfoH2>
+          Phone:
+          <InfoLink href="tel:240-319-3593">
+            240-319-3593
+          </InfoLink>
+        </InfoH2>
+        <InfoH3>
+          or use the following form:
+        </InfoH3>
+      </InfoSection>
+      <FormSection>
+        <ContactForm 
+          id='contact-form'
+          onSubmit={handleSubmit(onSubmit)}>
+          <FormWrapper>
+            <FormColumn1>
+              <ContactInput 
+                type='text' 
+                name='user_name' 
+                placeholder='name*' 
+                maxLength='30'
+                aria-invalid={errors.user_name ? 'true' : 'false'}
+                {...register('user_name', {required: 'required'})}/><br />
+              {errors.user_name && errors.user_name.type === 'required' && errors.user_name !== undefined && (
+                <ContactFormError>Name is required<br/></ContactFormError>
+              )}
+              <ContactInput 
+                type='text' 
+                name='user_email' 
+                placeholder='email*' 
+                aria-invalid={errors.user_email ? 'true' : 'false'}
+                {...register('user_email', {required: 'required'})}/><br />
+              {errors.user_email && errors.user_email.type === 'required' && errors.user_email !== undefined && (
+                <ContactFormError>email is required<br/></ContactFormError>
+              )}
+              <ContactInput 
+                type='text'
+                name='user_phone' 
+                placeholder='phone number' 
+                value={phoneNumber}
+                onChange={(e) => handlePhoneChange(e.target.value)}
+                aria-invalid={errors.user_phone ? 'true' : 'false'}
+                {...register}/>
+              </FormColumn1>
+              <FormColumn2>
+                <ContactMessage 
+                  type='text' 
+                  name='message' 
+                  placeholder='message*' 
+                  maxLength='1500'
+                  aria-invalid={errors.message ? 'true' : 'false'}
+                  {...register('message', {required: 'required'})} /><br />
+                {errors.message && errors.message.type === 'required' && errors.message !== undefined && (
+                  <ContactFormError>message is required<br/></ContactFormError>
+                  )}
+                <ContactFormTip>{messageCharsLeft}/1500 characters</ContactFormTip>
+              </FormColumn2>
+            </FormWrapper>
+          <ContactFormSubmit type='submit' value='Send' />
+          <ContactInput type='hidden' name='contact_number' value={contactNumber} />
+        </ContactForm>
+      </FormSection>
+    </ContactContainer>
   )
 }
 
