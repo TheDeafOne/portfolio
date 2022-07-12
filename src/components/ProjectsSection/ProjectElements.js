de90d12c859622ed01c3c14aaf9dcba03b73658c
TheDeafOne <woodburnkb20@gcc.edu>
2022-07-11 20:10:30 -0400

    dynamically fit columns

--- src/components/ProjectsSection/ProjectElements.js
+++ src/components/ProjectsSection/ProjectElements.js
@@ -1,20 +1,13 @@
 import styled from 'styled-components';
 
 export const ProjectsContainer = styled.div`
-    height: 800px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     background: #010606;
-
-    @media screen and (max-width: 768px) {
-        height: 1100px;
-    }
-
-    @media screen and (max-width: 480px) {
-        height: 1300px;
-    }
+    min-height: 100vh;
+    padding-bottom: 30px;
 `
 
 export const ProjectsH1 = styled.h1`
@@ -41,11 +34,11 @@ export const ProjectsWrapper = styled.div`
     margin: 0 auto;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
+    justify-content: center;
     grid-gap: 16px;
     padding: 0 50px;
     width: 100%;
-    height: 100%;
+
     @media screen and (max-width: 1000px) {
         grid-template-columns: 1fr 1fr;
     }
@@ -59,16 +52,25 @@ export const ProjectsWrapper = styled.div`
 export const ProjectCard = styled.div`
     background: #fff;
     display: flex;
+    margin: auto;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    /* box-shadow: - 1px 3px rgba(0,0,0,0.2); */
     position: relative;
     transition: all 0.2s ease-in-out;
     &:hover {
         transform: scale(0.95);
         transition: all 0.25s ease-in-out;
         cursor: pointer;
+    }
+
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        width: 70%;
     }
 `
 
