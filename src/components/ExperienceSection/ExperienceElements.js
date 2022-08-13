b6769388eefe16d150885440e4ef455fcbb3ed13
TheDeafOne <woodburnkb20@gcc.edu>
2022-08-13 15:52:57 -0400

    experience toggling

--- src/components/ExperienceSection/ExperienceElements.js
+++ src/components/ExperienceSection/ExperienceElements.js
@@ -1,6 +1,55 @@
 import styled from 'styled-components';
 
 export const ExperienceContainer = styled.div`
-    height: 100vh;
+    min-height: 100vh;
     background-color:black;
+`
+
+export const Timeline = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 25px;
+    display: grid;
+    grid-template-columns: 3px 1fr;
+    font-family: 'Fira Sans', sans-serif;
+    color: transparent;
+`
+
+export const Experience = styled.div`
+    margin-left: 20px;
+    margin-bottom: 50px;
+`
+
+export const Info = styled.div`
+    height: 0px;
+    background: blue;
+`
+
+export const Date = styled.div`
+    color: red;
+`
+
+export const Bar = styled.div`
+    position: relative;
+    background: white;
+    transition: ease-in-out 2s;
+`
+
+export const PointRow = styled.div`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    background-color: yellow;
+    width: 400px;
+`
+
+export const Point = styled.div`
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 30px;
+    height: 30px;
+    background: white;
+    border-radius: 50%;
+    cursor: pointer;
 `
--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
-  ExperienceContainer
+  ExperienceContainer,
+  Timeline,
+  Experience,
+  Info,
+  Date,
+  Bar,
+  Point,
+  PointRow
 } from './ExperienceElements';
 const ExperienceSection = () => {
+  const changeActiveExperience = async (experience) => {
+    if (document.getElementById(experience).style.height === '0px') {
+      document.getElementById(experience).style.height = '200px';
+    } else {
+      document.getElementById(experience).style.height = '0px';
+    }
+  } 
+
+  useEffect(() => {
+    document.getElementById('experience1').style.height = '200px';
+  }, [])
+
   return (
     <ExperienceContainer id='experience'>
+      <Timeline>
+        <Bar>
+          <PointRow>
+            <Point 
+              onClick={() => changeActiveExperience('experience1')}
+            />
+            <Date>
+              March 2022 - Present
+            </Date>
+          </PointRow>
+        </Bar>
+        <Experience>
+          
+          <Info id='experience1'>
 
+          </Info>
+        </Experience>
+        <Bar>
+          <PointRow>
+            <Point
+              onClick={() => changeActiveExperience('experience2')} 
+            />
+            <Date>
+                May 2021 - October 2021
+            </Date>
+          </PointRow>
+        </Bar>
+
+        <Experience>
+          <Info id='experience2'/>
+          
+        </Experience>
+        <Bar>
+          <PointRow>
+            <Point
+              onClick={() => changeActiveExperience('experience3')} 
+            />
+            <Date>
+                May 2021 - October 2021
+            </Date>
+          </PointRow>
+        </Bar>
+
+        <Experience>
+          <Info id='experience3'/>
+          
+        </Experience>
+        <Bar>
+          <PointRow>
+            <Point
+              onClick={() => changeActiveExperience('experience4')} 
+            />
+            <Date>
+                May 2021 - October 2021
+            </Date>
+          </PointRow>
+        </Bar>
+
+        <Experience>
+          <Info id='experience4'/>
+          
+        </Experience>
+
+      </Timeline>
     </ExperienceContainer>
   )
 }
